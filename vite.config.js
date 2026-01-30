72079734b7e852b903202418d612df658a956f87
MarcioPerusin <marcioperozin@gmail.com>
2026-01-29 22:45:42 -0300

    refactor: Refactors project sprint 14 = user authentication

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/web_project_around_auth/",
-  build: {
-    outDir: "docs",
-  },  
   plugins: [react()],
   server: {
     port: 3000,
   },
+  base: "/web_project_around_auth/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },  
 });
